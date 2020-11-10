6e907eec7fe6f1a173e597f431b41737087003e4
Denny da Vjncj <daVjncjCode@gmail.com>
2020-11-09 23:48:59 -0500

    developing buildReadMe function

--- develop/index.js
+++ develop/index.js
@@ -3,28 +3,61 @@ const effEss=require('fs');
 const utility=require('util');
 
 // array of questions for user
-const questions = [
-    {
-        type:'input',
-        name:'title',
-        message:'what is the title for your project?'
-    },
-    {
-        type:'input',
-        name:'description',
-        message:'tell us all about your project, be as descriptive as possible while making use of brevity'
-
-    },
-    {
-        type:'input',
-        name:'installInstructions',
-        message:''
-
-    },
-
-
-
-];
+const buildReadMe =()=>{
+    return inquire.prompt([
+        {
+            type:'input',
+            name:'github username',
+            message:'enter your github username'
+        },
+        {
+            type:'input',
+            name:'email',
+            message:'enter your email address'
+        },
+        {
+            type:'input',
+            name:'title',
+            message:'whats the title for this stellar application?'
+        },
+        {
+            type:'input',
+            name:'description',
+            message:'tell us all about your project, be as descriptive as possible while making use of brevity'
+    
+        },
+        {
+            type:'input',
+            name:'contents',
+            message:'provide a quick breakdown of how to traverse your project'
+    
+        },
+        {
+            type:'input',
+            name:'installation',
+            message:'provide clear instructions in setting up functionality for this project'
+    
+        },
+        {
+            type:'input',
+            name:'usage',
+            message:'tell prospective users how to make use of this application'
+    
+        },
+        {
+            type:'input',
+            name:'contributing',
+            message:'inform users how they may contribute to this project'
+    
+        },
+        {
+            type:'input',
+            name:'test',
+            message:'inform users how they may contribute to this project'
+    
+        }
+    ])
+}
 
 // function to write README file
 function writeToFile(fileName, data) {
